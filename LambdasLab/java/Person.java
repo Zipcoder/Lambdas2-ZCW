211fd2e985aba74f5828e7597bac9a7289ab0909
David Trombello <david@zipcoders-MacBook-Pro-96.local>
2019-11-26 12:05:11 -0500

    updating Person and test class

--- LambdasLab/java/Person.java
+++ LambdasLab/java/Person.java
@@ -1,2 +1,64 @@
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Person {
+
+    String name;
+    LocalDate birthday;
+    Sex gender;
+    String emailAddress;
+
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    public Person(String name, LocalDate birthday, Sex gender, String emailAddress) {
+        this.name = name;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.emailAddress = emailAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public Sex getGender() {
+        return gender;
+    }
+
+    public void setGender(Sex gender) {
+        this.gender = gender;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public int getAge(LocalDate birthday) {
+        LocalDate today = LocalDate.now();
+        Integer p = Period.between(birthday, today).getYears();
+        // OR
+        return p;
+    }
+
+    public void printPerson() {
+        // ...
+    }
 }
--- Test/java/PersonTest.java
+++ Test/java/PersonTest.java
@@ -1,3 +1,35 @@
-public class PersonTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+import java.time.LocalDate;
+import java.time.Month;
+
+public class PersonTest {
+    Person tp;
+
+    @Before
+    public void setUp() throws Exception {
+         tp = new Person ("John Doe", LocalDate.of(1976, Month.JANUARY, 10), Person.Sex.MALE, "johnDoe@hotmail.com");
+    }
+
+    @Test
+    void setName() {
+        String expectedName = "Pete Smith";
+        tp.setName("Pete Smith");
+        String actual = tp.getName();
+        Assert.assertEquals(expectedName, actual);
+    }
+
+    @Test
+    void setBirthday() {
+    }
+
+    @Test
+    void setGender() {
+    }
+
+    @Test
+    void setEmailAddress() {
+    }
 }
